50e45a4e1a681c94a64755aa0c7c29289f6798f9
MAGUETTE DIA <160591846+magoudia@users.noreply.github.com>
2025-10-27 10:35:56 +0000

    Diaz

--- site-iai-comp-main/vite.config.ts
+++ site-iai-comp-main/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ← Ajoute cette ligne
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
